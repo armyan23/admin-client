ce3a677636aa344fa60d1f29fffd7be891ba20c8
armyan23 <arm.davtyan1029@gmail.com>
2022-12-23 23:06:12 +0400

    Some image responsive

--- component/profile/ProfileImage.jsx
+++ component/profile/ProfileImage.jsx
@@ -3,10 +3,20 @@ import Image from "next/image";
 import Male from "public/assets/image/profile/male.jpg";
 import Female from "public/assets/image/profile/female.jpg";
 
-const ProfileImage = ({ url, type = "male", width = 250, height = 250 }) => {
-  return (
+const ProfileImage = ({ url, type = "male", width = 250, height = 320 }) => {
+  return url ? (
+    <div
+      style={{
+        backgroundImage: `url('${url}')`,
+        width: `${width}px`,
+        height: `${height}px`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+      }}
+    />
+  ) : (
     <Image
-      src={url ? url : type === "male" ? Male : Female}
+      src={type === "male" ? Male : Female}
       alt="ProfileImg"
       width={width}
       height={height}
